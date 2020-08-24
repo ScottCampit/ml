68a62d7809d344af01598ce7da7da5b7af3d1354
ScottCampit <scampit@umich.edu>
2020-08-24 10:50:14 -0400

    Edited regressor ensemble code

--- MATLAB/srv/costEvaluation.m
+++
@@ -1,8 +0,0 @@
-function J = costEvaluation(Ytrain, yhat)
-%% COSTEVALUATION Cost Evaluation
-% To evaluate the cost for our linear regressor, we'll use the mean squared 
-% error:
-% 
-% $$\mathrm{MSE}=\frac{1}{n}\sum_{i=1}^n {\left(y_i -\hat{y_i } \right)}^2$$
-    J = 1 / size(Ytrain, 1) * sum((yhat-Ytrain) .^ 2);
-end
--- MATLAB/srv/forwardProp.m
+++
@@ -1,5 +0,0 @@
-function yhat = forwardProp(X, beta, intercept)
-%% FORWARDPROP Forward propagation for linear regression
-% Detailed explanation of this function.
-    yhat = sum(X .* beta' + intercept, 2);
-end
